5cd422d464a2fea6424a02bfdd62f595ea4270f5
Schell Scivally <efsubenovex@gmail.com>
2019-12-13 10:16:21 -0800

    workflow stuff

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+export PATH=$PATH:$HOME/.cargo/bin
+
+if hash rustup 2>/dev/null; then
+    echo "Have rustup, skipping installation..."
+else
+    echo "Installing rustup..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+if hash wasm-pack 2>/dev/null; then
+    echo "Have wasm-pack, skipping installation..."
+else
+    echo "Installing wasm-pack..."
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
+cargo build || exit 1
+cargo test || exit 1
+cargo doc || exit 1
+cd mogwai
+cargo publish --dry-run || exit 1
+cd ..
+
+echo "Done building on ref ${GITHUB_REF}"
