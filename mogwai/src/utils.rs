afe175fb3ef0d41ff5963dd28b81ef7e83b02232
Schell Scivally <efsubenovex@gmail.com>
2020-01-10 21:45:04 -0800

    request_animation frame

--- mogwai/src/utils.rs
+++ mogwai/src/utils.rs
@@ -50,6 +50,32 @@ where
   invalidate
 }
 
+fn req_animation_frame(f: &Closure<dyn FnMut()>) {
+  window()
+    .request_animation_frame(f.as_ref().unchecked_ref())
+    .expect("should register `requestAnimationFrame` OK");
+}
+
+pub fn request_animation_frame<F>(mut logic: F)
+where
+  F: FnMut() -> bool + 'static
+{
+  // https://rustwasm.github.io/wasm-bindgen/examples/request-animation-frame.html#srclibrs
+  let f = Rc::new(RefCell::new(None));
+  let g = f.clone();
+
+  *g.borrow_mut()
+    = Some(Closure::wrap(Box::new(move || {
+      let should_continue = logic();
+      if should_continue {
+        req_animation_frame(f.borrow().as_ref().unwrap());
+      }
+    }) as Box<dyn FnMut()>));
+
+  req_animation_frame(g.borrow().as_ref().unwrap());
+  return;
+}
+
 /// Insert a child element into a parant element.
 pub fn nest_gizmos(parent: &Gizmo, child: &Gizmo) -> Result<(), JsValue> {
   let child =
