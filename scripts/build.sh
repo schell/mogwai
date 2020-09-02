dc8883379fe16a8c970d31bce033f1fabc69b95a
Schell Scivally <efsubenovex@gmail.com>
2020-09-02 15:02:37 -0700

    remove publish dryrun

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,5 @@ ROOT="$(git rev-parse --show-toplevel)"
 . $ROOT/scripts/common.sh
 
 cargo build || exit 1
-cd mogwai
-cargo publish --dry-run || exit 1
-cd ..
 
 echo "Done building on ref ${GITHUB_REF}"
