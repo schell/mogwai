da5dbe793997117b3860b344135a9c68c1351b17
Schell Scivally <efsubenovex@gmail.com>
2020-10-04 10:16:17 -0700

    mdbook uses its own build target

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,8 @@ ROOT="$(git rev-parse --show-toplevel)"
 cargo test || exit 1
 cargo doc || exit 1
 wasm-pack test --firefox --headless mogwai || exit 1
-mdbook test cookbook -L target/debug/deps || exit 1
+
+cargo build --package cookbook --target-dir cookbook_target --lib || exit 1
+mdbook test cookbook -L cookbook_target/debug/deps || exit 1
 
 echo "Done testing on ref ${GITHUB_REF}"
