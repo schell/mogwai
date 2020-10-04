accd0181faa8dbfc37b6cae60999611f1f36cecf
Bryan J Swift <mail@bryanwrit.es>
2020-10-04 16:55:48 -0400

    Testing out a multipage build (#44)
    * Create an example with multiple routes
    
    Each route renders its content based only on the `Route`.
    
    * Add logging and additional log levels to aid debugging/exploring
    
    * Eliminate the `Out::SkipRender` message
    
    There's no need for it, if we do not send a message with new route
    information not attempt will be made to update the view. Thus
    `SkipRender` is logically the equivalent of no message.
    
    * Remove log statements
    
    The log statements were a way to explore the sequence of events
    surrounding element drops but are not useful for "app" operation.
    
    * Use `From<Route>` to create a `View`
    
    Rather than calling an `App::render` function. `render` sounds too much
    like a pattern from other frameworks that does not match this mental
    model.
    
    * Return `ViewBuilder` from route functions
    
    Use `From` to enable conversion from `Route` to `ViewBuilder` and from
    `ViewBuilder` to `View`. The `Route` -> `ViewBuilder` is used for
    initial render. `Route` -> `View` is used when a new `Route` is received
    on the `Receiver`.

---
+++ examples/multipage/README.md
@@ -0,0 +1,4 @@
+# Multi Screen Example
+
+A simple example that serves two "routes". Each "route" changes the main
+`View` displayed by the `Gizmo`.
---
+++ examples/multipage/src/app.rs
@@ -0,0 +1,88 @@
+use crate::routes;
+use mogwai::prelude::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Route {
+    Home,
+    NotFound,
+}
+
+impl From<Route> for View<HtmlElement> {
+    fn from(route: Route) -> Self {
+        ViewBuilder::from(route).into()
+    }
+}
+
+impl From<Route> for ViewBuilder<HtmlElement> {
+    fn from(route: Route) -> Self {
+        match route {
+            Route::Home => routes::home(),
+            Route::NotFound => routes::not_found(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Out {
+    Render(Route),
+    RenderClicks(i32),
+}
+
+#[derive(Debug)]
+pub struct App {
+    click_count: i32,
+    current_route: Route,
+}
+
+impl App {
+    pub fn new() -> Self {
+        App {
+            click_count: 0,
+            current_route: Route::Home,
+        }
+    }
+}
+
+impl Component for App {
+    type ModelMsg = Route;
+    type ViewMsg = Out;
+    type DomNode = HtmlElement;
+
+    fn update(&mut self, msg: &Route, tx_view: &Transmitter<Out>, _sub: &Subscriber<Route>) {
+        if self.current_route != *msg {
+            self.current_route = msg.clone();
+            tx_view.send(&Out::Render(self.current_route));
+        }
+        self.click_count += 1;
+        tx_view.send(&Out::RenderClicks(self.click_count));
+    }
+
+    #[allow(unused_braces)]
+    fn view(&self, tx: &Transmitter<Route>, rx: &Receiver<Out>) -> ViewBuilder<HtmlElement> {
+        let rx_text = rx.branch_filter_map(|msg| match msg {
+            Out::RenderClicks(count) if count == &1 => Some(format!("{} time", count)),
+            Out::RenderClicks(count) => Some(format!("{} times", count)),
+            _ => None,
+        });
+        let rx_main = rx.branch_filter_map(|msg| match msg {
+            Out::Render(route) => Some(View::<HtmlElement>::from(*route)),
+            _ => None,
+        });
+        let mut contents = ViewBuilder::from(self.current_route);
+        contents.replaces = vec![rx_main];
+        builder! {
+            <div class="root">
+                <nav>
+                    <button on:click=tx.contra_map(|_| Route::Home)>
+                        "Home"
+                    </button>
+                    <button on:click=tx.contra_map(|_| Route::NotFound)>
+                        "Not Found"
+                    </button>
+                </nav>
+                <p>{("0 times", rx_text)}</p>
+                {contents}
+            </div>
+        }
+    }
+}
