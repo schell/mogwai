f046634b32cfe9cb1521afcab22b9b3ede75e641
Bryan Swift <bryan@bryanjswift.com>
2020-10-11 16:47:45 -0400

    Add `api::get_game_list` to retrieve list of game ids

--- examples/multipage/src/api.rs
+++ examples/multipage/src/api.rs
@@ -67,3 +67,35 @@ pub async fn get_game(game_id: String) -> Result<GameState, FetchError> {
     // Use serde to parse the JSON into a struct.
     json.into_serde().map_err(|_| FetchError::ParseError)
 }
+
+pub async fn get_game_list() -> Result<Vec<String>, FetchError> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("{}/game", API_URL);
+
+    let request = Request::new_with_str_and_init(&url, &opts)
+        .map_err(|_| FetchError::RequestCreateError)?;
+
+    request
+        .headers()
+        .set("Accept", "application/json")
+        .map_err(|_| FetchError::RequestHeaderSetError)?;
+
+    let window = utils::window();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request))
+        .await
+        .map_err(|_| FetchError::FetchError)?;
+
+    // `resp_value` is a `Response` object.
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json().map_err(|_| FetchError::FetchError)?)
+        .await
+        .map_err(|_| FetchError::FetchError)?;
+
+    // Use serde to parse the JSON into a struct.
+    json.into_serde().map_err(|_| FetchError::ParseError)
+}
