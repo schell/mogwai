381345279dbe1b3af35181ba2b0ed5e2904e260f
Bryan Swift <bryan@bryanjswift.com>
2020-10-12 12:44:14 -0400

    Use namespaces to clarify intent

--- examples/multipage/src/lib.rs
+++ examples/multipage/src/lib.rs
@@ -4,9 +4,7 @@ mod components;
 mod routes;
 
 use crate::app::App;
-use log::{trace, Level};
 use mogwai::prelude::*;
-use std::panic;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -17,22 +15,21 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
-    console_log::init_with_level(Level::Trace).expect("could not init console_log");
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init_with_level(log::Level::Trace).expect("could not init console_log");
 
     if cfg!(debug_assertions) {
-        trace!("Hello from debug mogwai-multipage");
+        log::trace!("Hello from debug mogwai-multipage");
     } else {
-        trace!("Hello from release mogwai-multipage");
+        log::trace!("Hello from release mogwai-multipage");
     }
 
     // Create our app's view by hydrating a gizmo from an initial state
-    let root = App::new();
-    let root_app: Gizmo<App> = Gizmo::new(root);
+    let root: Gizmo<App> = Gizmo::new(App::new());
 
     // Hand the app's view ownership to the window so it never
     // goes out of scope
-    View::from(root_app).run()
+    View::from(root).run()
 }
 
 #[cfg(test)]
