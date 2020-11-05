1303f9800e35b637bddbb8d72c824d68877459d4
Schell Scivally <efsubenovex@gmail.com>
2020-11-05 10:45:04 -0800

    only some nodes are voidable

--- mogwai/src/ssr.rs
+++ mogwai/src/ssr.rs
@@ -1,3 +1,44 @@
+//! Provides string rendering for server-side mogwai nodes.
+
+/// Only certain nodes can be "void" - which means written as <tag /> when
+/// the node contains no children. Writing non-void nodes in void notation
+/// does some spooky things to the DOM at parse-time.
+///
+/// From https://riptutorial.com/html/example/4736/void-elements
+/// HTML 4.01/XHTML 1.0 Strict includes the following void elements:
+///
+///     area - clickable, defined area in an image
+///     base - specifies a base URL from which all links base
+///     br - line break
+///     col - column in a table [deprecated]
+///     hr - horizontal rule (line)
+///     img - image
+///     input - field where users enter data
+///     link - links an external resource to the document
+///     meta - provides information about the document
+///     param - defines parameters for plugins
+///
+///     HTML 5 standards include all non-deprecated tags from the previous list and
+///
+///     command - represents a command users can invoke [obsolete]
+///     keygen - facilitates public key generation for web certificates [deprecated]
+///     source - specifies media sources for picture, audio, and video elements
+fn tag_is_voidable(tag: &str) -> bool {
+    tag == "area"
+        || tag == "base"
+        || tag == "br"
+        || tag == "col"
+        || tag == "hr"
+        || tag == "img"
+        || tag == "input"
+        || tag == "link"
+        || tag == "meta"
+        || tag == "param"
+        || tag == "command"
+        || tag == "keygen"
+        || tag == "source"
+}
+
 #[derive(Debug)]
 pub enum Node {
     Text(String),
@@ -31,9 +72,17 @@ impl From<Node> for String {
 
                 if children.is_empty() {
                     if attributes.is_empty() {
-                        format!("<{} />", name)
+                        if tag_is_voidable(&name) {
+                            format!("<{} />", name)
+                        } else {
+                            format!("<{}></{}>", name, name)
+                        }
                     } else {
-                        format!("<{} {} />", name, atts)
+                        if tag_is_voidable(&name) {
+                            format!("<{} {} />", name, atts)
+                        } else {
+                            format!("<{} {}></{}>", name, atts, name)
+                        }
                     }
                 } else {
                     let kids = children
