aea019a6d74da179c1c9709aa409dd7105e880c5
Schell Scivally <efsubenovex@gmail.com>
2020-11-05 12:33:43 -0800

    remove git stuff from mogwai-html-macro

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
+set -e
 
 ROOT="$(git rev-parse --show-toplevel)"
 . $ROOT/scripts/common.sh
 
 TOKEN=$1
 
+echo "publishing mogwai-html-macro"
 cd $ROOT
 cd crates/mogwai-html-macro
 cargo publish --dry-run --token $TOKEN
 
+echo "publishing mogwai"
 cd $ROOT
 cd mogwai
 cargo publish --dry-run --token $TOKEN
 
+echo "publishing mogwai-hydrator"
 cd $ROOT
 cd crates/mogwai-hydrator
 cargo publish --dry-run --token $TOKEN
