2b85a2ef37f4fca1ba7c1e25eb99316136f232c9
Schell Carl Scivally <efsubenovex@gmail.com>
2020-11-06 14:20:42 -0800

    Push cookbook (#63)
    * trim cookbook
    
    * add cookbook pages to doc test

--- cookbook/src/lib.rs
+++ cookbook/src/lib.rs
@@ -1,10 +1,15 @@
 //! # Cookbook
 
 #[cfg(doctest)]
-doc_comment::doctest!("component.md", component_md);
-
+doc_comment::doctest!("intro.md", intro);
 #[cfg(doctest)]
-doc_comment::doctest!("nest_component.md", nest_component_md);
+doc_comment::doctest!("new_project.md", new_project);
+#[cfg(doctest)]
+doc_comment::doctest!("component.md", component);
+#[cfg(doctest)]
+doc_comment::doctest!("nest_component.md", nest_component);
+#[cfg(doctest)]
+doc_comment::doctest!("rsx.md", rsx);
 
 /// A cookbook
 pub fn cookbook() {
