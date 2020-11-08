af94a12829b561fb8060899daf2b41adad25ba80
Schell Scivally <efsubenovex@gmail.com>
2020-11-08 07:39:43 -0800

    only publish when versions don't match

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,17 +10,24 @@ publish() {
     DIR=$1
     CRATE=`echo $DIR | cut -d'/' -f2-`
     echo "publishing $CRATE from $DIR"
-    cargo search $CRATE
-    cd $ROOT
-    cd $DIR
-    cargo publish --token $TOKEN
-    if [ "$?" = "101" ]; then
-        echo "  no dice!"
+    REMOTE_VERSION=`cargo search $CRATE | grep "$CRATE = " | cut -d'"' -f2`
+    echo "  remote version: '$REMOTE_VERSION'"
+    LOCAL_VERSION=`cat $DIR/Cargo.toml | grep -E '^version ?=' | cut -d$'\n' -f1 | cut -d'"' -f2`
+    echo "  local version: '$LOCAL_VERSION'"
+    if [ $REMOTE_VERSION = $LOCAL_VERSION ]; then
+        echo "  cargo has the same version - done!"
     else
-        sleep $SLEEP
-        echo "  done!"
+        cd $ROOT
+        cd $DIR
+        cargo publish --token $TOKEN
+        if [ "$?" = "101" ]; then
+            echo "  no dice!"
+        else
+            sleep $SLEEP
+            echo "  done!"
+        fi
+        cd $ROOT
     fi
-    cd $ROOT
 }
 
 DIRS="crates/mogwai-html-macro mogwai crates/mogwai-hydrator"
