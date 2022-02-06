2f5c1d7e8c1abe79c743e85d4e65277e09bdf795
Schell Carl Scivally <efsubenovex@gmail.com>
2022-02-06 14:11:44 +1300

    don't build smol on wasm dev-deps

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,16 @@
 ROOT="$(git rev-parse --show-toplevel)"
 . $ROOT/scripts/common.sh
 
+echo "### CARGO TEST"
 cargo test || exit 1
+
+echo "### CARGO DOC"
 cargo doc || exit 1
 
+echo "### WASM-PACK TEST"
 wasm-pack test --firefox --headless crates/mogwai || exit 1
+
+echo "### WASM-PACK TEST HYDRATOR"
 wasm-pack test --firefox --headless crates/mogwai-hydrator || exit 1
 
 echo "Done testing on ref ${GITHUB_REF}"
