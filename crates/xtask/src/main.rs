93578200dc655a6d34402fb7f38efb1a70d6e7d4
Schell Carl Scivally <efsubenovex@gmail.com>
2023-01-07 10:44:54 +1300

    can push cookbook

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -8,6 +8,114 @@ use clap::{Parser, Subcommand};
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
+fn build_example(name: Option<String>) -> anyhow::Result<()> {
+    let root = PathBuf::from(get_root_prefix()?);
+    tracing::debug!("root: '{}'", root.display());
+    anyhow::ensure!(root.is_dir(), "root is not a dir");
+    anyhow::ensure!(root.exists(), "root dir does not exist");
+
+    let book_examples_dir = PathBuf::from(&root).join("book_examples");
+    if !book_examples_dir.exists() {
+        std::fs::create_dir(&book_examples_dir).context("could not create book examples dir")?;
+    } else {
+        tracing::warn!("book_examples dir already exists, will overwrite");
+    }
+
+    let examples_dir = PathBuf::from(&root).join("examples");
+    let examples = std::fs::read_dir(&examples_dir)?;
+    for example in examples {
+        let entry = example?;
+        let example_path = entry.path();
+        let example_name = example_path
+            .file_name()
+            .context("could not get example name")?
+            .to_str()
+            .context("could not make str")?;
+        let excludes = [".DS_Store", "multipage", "sandbox", "focus-follower"];
+        if excludes.contains(&example_name) {
+            continue;
+        }
+        if let Some(name) = name.as_ref() {
+            if name.as_str() != example_name {
+                continue;
+            }
+        }
+
+        tracing::info!(
+            "building example {:?} from {}",
+            example_name,
+            examples_dir.display()
+        );
+        duct::cmd!(
+            "wasm-pack",
+            "build",
+            "--debug",
+            "--target",
+            "web",
+            &example_path
+        )
+        .run()
+        .context("could not build example")?;
+        let example_destination = &book_examples_dir.join(example_name);
+        if example_destination.exists() {
+            tracing::warn!(
+                "destination {} already exists - removing it first",
+                example_destination.display()
+            );
+            duct::cmd!("rm", "-rf", &example_destination)
+                .run()
+                .context("could not remove stale destination")?;
+        }
+        std::fs::create_dir_all(&example_destination)
+            .context("could not create example destination")?;
+        std::env::set_current_dir(&example_path).context("could not cd")?;
+
+        duct::cmd!("cp", "-R", "index.html", "pkg", &example_destination)
+            .run()
+            .context("could not copy files into place")?;
+        let style = PathBuf::from("style.css");
+        if style.exists() {
+            duct::cmd!("cp", &style, &example_destination)
+                .run()
+                .context("could not copy style.css")?;
+        }
+    }
+
+    tracing::info!("done building");
+
+    Ok(())
+}
+
+fn build_cookbook(cookbook_root_path: Option<PathBuf>) -> anyhow::Result<()> {
+    let root = PathBuf::from(get_root_prefix()?);
+    std::env::set_current_dir(&root).context("could not cd to root")?;
+
+    let build_cookbook_cmd = duct::cmd!("mdbook", "build", "cookbook");
+    let build_cookbook_cmd = if let Some(path) = cookbook_root_path {
+        tracing::info!("building cookbook with root path '{}'", path.display());
+        build_cookbook_cmd.env(
+            "MDBOOK_preprocessor__variables__variables__cookbookroot",
+            &format!("{}", path.display()),
+        )
+    } else {
+        build_cookbook_cmd
+    };
+    build_cookbook_cmd
+        .run()
+        .context("could not build cookbook")?;
+
+    duct::cmd!(
+        "cp",
+        "-R",
+        &root.join("book_examples"),
+        "cookbook/book/html/examples"
+    )
+    .run()
+    .context("could not copy examples into book")?;
+
+    Ok(())
+}
+
 #[derive(Parser)]
 #[clap(author, version, about, subcommand_required = true)]
 struct Cli {
@@ -24,6 +132,32 @@ struct Cli {
 }
 
 #[derive(Parser)]
+struct Cookbook {
+    /// Skip building the examples.
+    #[clap(long)]
+    skip_examples: bool,
+    /// Set the root path of the cookbook
+    #[clap(long)]
+    root_path: Option<PathBuf>,
+}
+
+impl Cookbook {
+    fn build(self) -> anyhow::Result<()> {
+        let Cookbook {
+            skip_examples,
+            root_path,
+        } = self;
+        if !skip_examples {
+            build_example(None)?;
+        } else {
+            tracing::info!("skipping building examples");
+        }
+
+        build_cookbook(root_path)
+    }
+}
+
+#[derive(Parser)]
 enum Artifact {
     /// One or all of the examples
     Example {
@@ -33,138 +167,14 @@ enum Artifact {
     },
 
     /// The cookbook
-    Cookbook {
-        /// Skip building the examples.
-        #[clap(long)]
-        skip_examples: bool,
-        /// Set the root path of the cookbook
-        #[clap(long)]
-        root_path: Option<PathBuf>,
-    },
+    Cookbook(Cookbook),
 }
 
 impl Artifact {
-    fn build_example(name: Option<String>) -> anyhow::Result<()> {
-        let root = PathBuf::from(get_root_prefix()?);
-        tracing::debug!("root: '{}'", root.display());
-        anyhow::ensure!(root.is_dir(), "root is not a dir");
-        anyhow::ensure!(root.exists(), "root dir does not exist");
-
-        let book_examples_dir = PathBuf::from(&root).join("book_examples");
-        if !book_examples_dir.exists() {
-            std::fs::create_dir(&book_examples_dir)
-                .context("could not create book examples dir")?;
-        } else {
-            tracing::warn!("book_examples dir already exists, will overwrite");
-        }
-
-        let examples_dir = PathBuf::from(&root).join("examples");
-        let examples = std::fs::read_dir(&examples_dir)?;
-        for example in examples {
-            let entry = example?;
-            let example_path = entry.path();
-            let example_name = example_path
-                .file_name()
-                .context("could not get example name")?
-                .to_str()
-                .context("could not make str")?;
-            let excludes = [".DS_Store", "multipage", "sandbox", "focus-follower"];
-            if excludes.contains(&example_name) {
-                continue;
-            }
-            if let Some(name) = name.as_ref() {
-                if name.as_str() != example_name {
-                    continue;
-                }
-            }
-
-            tracing::info!(
-                "building example {:?} from {}",
-                example_name,
-                examples_dir.display()
-            );
-            duct::cmd!(
-                "wasm-pack",
-                "build",
-                "--debug",
-                "--target",
-                "web",
-                &example_path
-            )
-            .run()
-            .context("could not build example")?;
-            let example_destination = &book_examples_dir.join(example_name);
-            if example_destination.exists() {
-                tracing::warn!(
-                    "destination {} already exists - removing it first",
-                    example_destination.display()
-                );
-                duct::cmd!("rm", "-rf", &example_destination)
-                    .run()
-                    .context("could not remove stale destination")?;
-            }
-            std::fs::create_dir_all(&example_destination)
-                .context("could not create example destination")?;
-            std::env::set_current_dir(&example_path).context("could not cd")?;
-
-            duct::cmd!("cp", "-R", "index.html", "pkg", &example_destination)
-                .run()
-                .context("could not copy files into place")?;
-            let style = PathBuf::from("style.css");
-            if style.exists() {
-                duct::cmd!("cp", &style, &example_destination)
-                    .run()
-                    .context("could not copy style.css")?;
-            }
-        }
-
-        tracing::info!("done building");
-
-        Ok(())
-    }
-
-    fn build_cookbook(cookbook_root_path: Option<PathBuf>) -> anyhow::Result<()> {
-        let root = PathBuf::from(get_root_prefix()?);
-        std::env::set_current_dir(&root).context("could not cd to root")?;
-
-        let build_cookbook_cmd = duct::cmd!("mdbook", "build", "cookbook");
-        let build_cookbook_cmd = if let Some(path) = cookbook_root_path {
-            tracing::info!("building cookbook with root path '{}'", path.display());
-            build_cookbook_cmd
-                .env(
-                    "MDBOOK_preprocessor__variables__variables__cookbookroot",
-                    &format!("{}", path.display()),
-                )
-        } else {
-            build_cookbook_cmd
-        };
-        build_cookbook_cmd
-            .run()
-            .context("could not build cookbook")?;
-
-        duct::cmd!(
-            "cp",
-            "-R",
-            &root.join("book_examples"),
-            "cookbook/book/html/examples"
-        )
-        .run()
-        .context("could not copy examples into book")?;
-
-        Ok(())
-    }
-
     fn build(self) -> anyhow::Result<()> {
         match self {
-            Artifact::Example { name } => Self::build_example(name),
-            Artifact::Cookbook { skip_examples, root_path } => {
-                if !skip_examples {
-                    Self::build_example(None)?;
-                } else {
-                    tracing::info!("skipping building examples");
-                }
-                Self::build_cookbook(root_path)
-            }
+            Artifact::Example { name } => build_example(name),
+            Artifact::Cookbook(cookbook) => cookbook.build(),
         }
     }
 }
@@ -280,14 +290,9 @@ impl Test {
             anyhow::ensure!(Path::new("gentest").exists(), "gentest does not exist");
 
             tracing::info!("building gentest");
-            duct::cmd!(
-                "wasm-pack",
-                "build",
-                "--target",
-                "web"
-            )
-            .dir("gentest")
-            .run()?;
+            duct::cmd!("wasm-pack", "build", "--target", "web")
+                .dir("gentest")
+                .run()?;
 
             tracing::info!("cleaning up gentest (mogwai-template test dir)");
             std::fs::remove_dir_all(Path::new("gentest"))?;
@@ -304,6 +309,15 @@ enum Command {
     Build(Artifact),
     /// Test everything
     Test(Test),
+    /// Push the cookbook to AWS
+    PushCookbook {
+        /// Path to the s3 bucket we are pushing the cookbook to
+        #[clap(long, default_value = "s3://zyghost.com/guides/mogwai-cookbook")]
+        s3_path: String,
+
+        #[clap(flatten)]
+        cookbook: Cookbook,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
@@ -328,6 +342,24 @@ fn main() -> anyhow::Result<()> {
     match cli.command {
         Command::Build(artifact) => artifact.build()?,
         Command::Test(test) => test.run()?,
+        Command::PushCookbook { s3_path, cookbook } => {
+            if !have_program("aws") {
+                anyhow::bail!("missing 'aws' - please install 'aws' cli tool");
+            }
+
+            cookbook.build()?;
+
+            duct::cmd!(
+                "aws",
+                "s3",
+                "sync",
+                "cookbook/book/html",
+                &s3_path,
+                "--acl",
+                "public-read"
+            )
+            .run()?;
+        }
     }
 
     Ok(())
