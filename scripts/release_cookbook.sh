95510e9f09a07e4ff229f4377cc198a69d005dcc
Schell Carl Scivally <efsubenovex@gmail.com>
2023-01-07 10:59:49 +1300

    remove superflous scripts and workflows

--- scripts/release_cookbook.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-ROOT="$(git rev-parse --show-toplevel)"
-. $ROOT/scripts/build_cookbook.sh
-
-aws s3 sync cookbook/book/html s3://zyghost.com/guides/mogwai-cookbook --acl public-read
